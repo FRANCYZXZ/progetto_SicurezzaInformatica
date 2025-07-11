b0b312a294e8dd3253ec232fafddabfb234ad77d
Francesco Rondini <frondini23@gmail.com>
2025-07-11 13:28:11 +0200

    Corretto un commento

--- seed.py
+++ seed.py
@@ -17,22 +17,23 @@ def extract_entropy(audio_samples):
     byte_array = bytearray(int(b, 2) for b in byte_chunks if len(b) == 8)     # Converte ciascun gruppo di 8 bit in un intero
     return byte_array
 
-def save_seed(seed_bytes, timestamp):
+def save_seed(seed, timestamp):
     """
     Salva il seed crittografico calcolato in un file .txt
 
     Args:
-        seed_bytes: hash del seed
+        seed: bytearray contenente l'entropia estratta dai campioni audio
         timestamp: identificativo legato alla registrazione
+    
+    Azioni:
+        - Crea la cartella di destinazione se non esiste
+        - Salva il seed in un file di testo
 
-    Returns:
-        str: percorso del file seed generato
     """
     os.makedirs(FOLDER_SEEDS, exist_ok=True)
     filename = os.path.join(FOLDER_SEEDS, f"seed_{timestamp}.txt")
     
     with open(filename, "w") as f:
-        f.write(seed_bytes.hex())
+        f.write(seed.hex())
 
     print(f"Seed salvato in: seeds/seed_{timestamp}.txt")
-    return filename
